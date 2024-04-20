2474943bd4895619f3271cfc3815cb55a02f0236
integerincrement <87837473+integerincrement@users.noreply.github.com>
2024-04-19 21:46:52 -0400

    added rotation script

--- Assets/Scripts/clock.cs
+++ Assets/Scripts/clock.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class clock : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Clock : UnityEngine.MonoBehaviour  {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // one hour on a clock is 30 degrees of rotation
+    const float hoursToDegrees = -30f;
+    const float minutesToDegrees = -6f;
+    const float secondsToDegrees = -6f;
+
+    [SerializeField]
+    Transform hoursPivot , minutesPivot, secondsPivot;
+
+    private void Update()    {
+        DateTime time = DateTime.Now;
+
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * time.Hour);
+
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * time.Minute);
+
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * time.Second);
     }
 }
